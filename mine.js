055520c65e973a2130a4ccce3fe7370bad4c11d3
m-maher <ronaldooo3@yahoo.com>
2018-08-28 19:22:56 +0200

    Add google map API to the website

--- mine.js
+++ mine.js
@@ -125,3 +125,14 @@ $(document).ready(function(){
     })
 })
 
+
+// Google Map
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
